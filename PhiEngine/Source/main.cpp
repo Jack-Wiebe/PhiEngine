00775c0cd98426f34d2a32a52b80e2a8afbcc154
JamieGB92 <jamie.g.copeland@gmail.com>
2018-01-31 12:27:03 -0500

    woot

--- PhiEngine/Source/main.cpp
+++ PhiEngine/Source/main.cpp
@@ -1,5 +1,5 @@
 #include "EngineInitialization.h"
-#include <SFML\Graphics.hpp>
+
 using namespace std;
 
 void main(int argc, char **argv)
@@ -23,18 +23,6 @@ void main(int argc, char **argv)
 
 	DWORD Success = ReadCPUSpeed();
 	system("pause");
-	sf::RenderWindow window({ 1024,769 }, "wuddup wit it");
-	while (window.isOpen())
-	{
-		sf::Event m_event;
-		while (window.pollEvent(m_event))
-		{
-			if (m_event.type == sf::Event::Closed)
-			{
-				window.close();
-			}
-		}
-		window.clear();
-		window.display();
-	}
+
+	
 }
