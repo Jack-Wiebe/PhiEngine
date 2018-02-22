f78a080e3e7e3ea5b5d317e136a7f3342a0c6d76
JamieGB92 <jamie.g.copeland@gmail.com>
2018-02-22 13:48:34 -0500

    Added scene graph funtionality to GameObject class

--- PhiEngine/Source/Engine/GameObject.cpp
+++ PhiEngine/Source/Engine/GameObject.cpp
@@ -4,9 +4,64 @@
 
 GameObject::GameObject()
 {
+	m_Parent = NULL;
 }
 
 
-GameObject::~GameObject()
+GameObject::~GameObject(void)
 {
+	for (unsigned int i = 0; i < children.size(); i++)
+	{
+	
+		delete children[i];
+		
+	}
+}
+
+void GameObject::setTransform(const sf::Transform & transform)
+{
+	m_transform = transform;
+}
+
+sf::Transform GameObject::GetTransform()
+{
+	return m_transform;
+}
+
+sf::Transform GameObject::GetWorldTransform()
+{
+	return m_worldTransform;
+}
+
+void GameObject::SetParent(GameObject& parent)
+{
+	m_Parent = &parent;
+	m_Parent->f_AddChild(this);
+}
+
+void GameObject::f_AddChild(GameObject* child)
+{
+	children.push_back(child);
+	child->m_Parent = this;
+}
+
+void GameObject::LateUpdate()
+{
+}
+
+void GameObject::Update(float msec)
+{
+	if (m_Parent)
+	{
+		m_worldTransform = m_Parent->m_worldTransform*m_transform;
+	}
+	else
+	{
+		m_worldTransform = m_transform;
+	}
+	for (std::vector<GameObject*>::iterator i = children.begin;
+		i != children.end(); i++)
+	{
+		(*i)->Update(msec);
+	}
 }
--- PhiEngine/Source/Engine/GameObject.h
+++ PhiEngine/Source/Engine/GameObject.h
@@ -1,17 +1,27 @@
 #pragma once
+#include <SFML\Graphics\Transform.hpp>
+#include <vector>
 class GameObject
 {
 public:
 	GameObject();
-	~GameObject();
+	~GameObject(void);
 
-	void LateUpdate();
-	void Update(float msec);
+	void setTransform(const sf::Transform &transform);
+	sf::Transform GetTransform();
+	sf::Transform GetWorldTransform();
 
-	void AddChild();
+	void SetParent(GameObject& parent);
+	void f_AddChild(GameObject* child);
 
+	virtual void LateUpdate();
+	virtual void Update(float msec);
 
-
+protected:
+	GameObject* m_Parent;
+	sf::Transform m_worldTransform;
+	sf::Transform m_transform;
+	std::vector<GameObject*> children;
 
 private:
 
