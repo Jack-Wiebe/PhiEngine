640f49f9fcb5966c3df5c116d9dd08aa7bec5b44
JamieGB92 <jamie.g.copeland@gmail.com>
2018-03-21 11:17:13 -0400

    finished physics component

--- PhiEngine/Source/Component/PhysicsComponent.h
+++ PhiEngine/Source/Component/PhysicsComponent.h
@@ -4,6 +4,7 @@
 #include <SFML\System\Vector2.hpp>
 #include <SFML\System\Vector3.hpp>
 #include "..\Engine\PhysicsEngine.h"
+#include "..\Engine\GameObject.h"
 #include "..\Component\TransformComponent.h"
 
 
@@ -17,11 +18,13 @@ public:
 	bool obeysGravity;
 	bool grounded;
 
+
+
 	TransformComponent transform;
 
 	sf::Vector2f currentVelocity;
 	sf::Vector2f maxVelocity;
-
+	sf::Vector2f gravity=sf::Vector2f(0.0f, -9.8f);
 	struct AABB
 	{
 		public:
@@ -31,20 +34,23 @@ public:
 
 	AABB aabb;
 
-	void Awake();
-	void Start();
+	void Awake() override;
+	void Start() override;
 	void Stop();
-	void Update();
-	void LateUpdate();
+	void Update() override;
+	void LateUpdate() override;
 
-	void AddForc(sf::Vector2f);
+	void AddForc(sf::Vector2f forces);
 	bool isGrounded();
 	void SetAABB();
 	void Integrate(float dt);
+	void SetGravity(sf::Vector2f newGravity);
 
 	~PhysicsComponent();
 private:
+	
 	sf::Vector2f totalForces;
 	PhysicsEngine* engine;
+	sf::Sprite* p_sprite;
 
 };
